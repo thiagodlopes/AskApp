a3dbba0e452cefebce5c174b396f13efa8f140b0
Thiago Lopes <thiagodaniellopes99@gmail.com>
2021-08-12 15:07:07 -0300

    Adiciona bottom navigation

---
+++ app/src/main/java/com/thdlopes/askapp/ui/CreateFragment.kt
@@ -0,0 +1,29 @@
+package com.thdlopes.askapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.thdlopes.askapp.R
+import com.thdlopes.askapp.databinding.FragmentCreateBinding
+
+class CreateFragment : Fragment() {
+
+    private var _binding: FragmentCreateBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentCreateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/com/thdlopes/askapp/ui/MainActivity.kt
+++ app/src/main/java/com/thdlopes/askapp/ui/MainActivity.kt
@@ -2,18 +2,48 @@ package com.thdlopes.askapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.thdlopes.askapp.R
+import com.thdlopes.askapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val createFragment = CreateFragment()
+    private val favoriteFragment = FavoriteFragment()
+    private val accountFragment = AccountFragment()
 
     private  lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
-        
+        changeFragment(homeFragment)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> changeFragment(homeFragment)
+                R.id.search -> changeFragment(searchFragment)
+                R.id.create -> changeFragment(createFragment)
+                R.id.favorite -> changeFragment(favoriteFragment)
+                R.id.account -> changeFragment(accountFragment)
+            }
+            true
+        }
+
     }
+
+    private fun changeFragment(frag: Fragment){
+        if (frag != null) {
+            val fragment = supportFragmentManager.beginTransaction()
+            fragment.replace(R.id.fragment_container,frag).commit()
+        }
+    }
+
 }
