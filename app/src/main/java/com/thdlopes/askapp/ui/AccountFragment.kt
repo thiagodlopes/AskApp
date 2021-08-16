577f8a8bea3580e98633750aa5e833b1e336e703
Thiago Lopes <thiagodaniellopes99@gmail.com>
2021-08-16 11:32:44 -0300

    Adiciona Cadastro com Conta Google

--- app/src/main/java/com/thdlopes/askapp/ui/AccountFragment.kt
+++ app/src/main/java/com/thdlopes/askapp/ui/AccountFragment.kt
@@ -1,16 +1,19 @@
 package com.thdlopes.askapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
 import com.thdlopes.askapp.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
 
     private var _binding: FragmentAccountBinding? = null
     private val binding get() = _binding!!
+    private  lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,4 +29,12 @@ class AccountFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.buttonLogout.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(getActivity(), LoginActivity::class.java))
+        }
+    }
 }
--- app/src/main/java/com/thdlopes/askapp/ui/LoginActivity.kt
+++ app/src/main/java/com/thdlopes/askapp/ui/LoginActivity.kt
@@ -3,16 +3,31 @@ package com.thdlopes.askapp.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.thdlopes.askapp.R
 import com.thdlopes.askapp.databinding.ActivityLoginBinding
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
     private  lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private companion object{
+        private const val RC_SIGN_IN = 100
+        private const val TAG = "GOOGLE_SIGN_IN"
+    }
 
     private var email = ""
     private var password = ""
@@ -44,6 +59,19 @@ class LoginActivity : AppCompatActivity() {
 
         binding.buttonLogin.setOnClickListener {
             validadeData()
+        }
+
+        //Google SignIn
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        binding.googleSignInButton.setOnClickListener {
+            Log.d(TAG, "onCreate: begin Google SignIn")
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, RC_SIGN_IN)
         }
     }
 
@@ -89,6 +117,54 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            Log.d(TAG, "onActivityResult: Google SignIn intent result")
+            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = accountTask.getResult(ApiException::class.java)
+                firebaseAuthWithGoogleAccount(account)
+            }
+            catch (e:Exception){
+                Log.d(TAG, "onActivityResult: ${e.message}")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogleAccount(account: GoogleSignInAccount?) {
+        Log.d(TAG, "firebaseAuthWithGoogleAccount: begin firebase auth with google account ")
+
+        val credential = GoogleAuthProvider.getCredential(account!!.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+                .addOnSuccessListener { authResult ->
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: LoggedIn")
+                    val firebaseUser = firebaseAuth.currentUser
+
+                    val uid = firebaseUser!!.uid
+                    val email = firebaseUser!!.email
+
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Uid: $uid")
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Email: $email")
+
+                    if (authResult.additionalUserInfo!!.isNewUser){
+                        Log.d(TAG, "firebaseAuthWithGoogleAccount: Account created... \n$email")
+                        Toast.makeText(this@LoginActivity, "Account created... \n$email", Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Log.d(TAG, "firebaseAuthWithGoogleAccount: Existing user... \n$email")
+                        Toast.makeText(this@LoginActivity, "Logged in... \n$email", Toast.LENGTH_SHORT).show()
+                    }
+                    checkUser()
+                }
+
+                .addOnFailureListener { e->
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Loggin Failed due to ${e.message}")
+                    Toast.makeText(this@LoginActivity, "Loggin Failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+                }
     }
 
 }
