1696118d5c51c43b5fa722247c5e0813d5037aef
Thiago Lopes <thiagodaniellopes99@gmail.com>
2021-09-17 17:13:22 -0300

    Adiciona votação beta

--- app/src/main/java/com/thdlopes/askapp/ui/SearchFragment.kt
+++ app/src/main/java/com/thdlopes/askapp/ui/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.thdlopes.askapp.ui
 
+import android.app.AlertDialog
 import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +10,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.thdlopes.askapp.R
@@ -86,6 +91,9 @@ class SearchFragment : Fragment() {
         })
 
         viewModel.getRealTimeUpdate("")
+
+        val itemTouchHelper = ItemTouchHelper(simpleCallback)
+        itemTouchHelper.attachToRecyclerView(binding.recyclerViewQuestions)
     }
 
     private fun getSelectedText(chipGroup: ChipGroup, id: Int): String {
@@ -96,6 +104,33 @@ class SearchFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private var simpleCallback = object : ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT.or(ItemTouchHelper.RIGHT)){
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            var position = viewHolder.adapterPosition
+            var currentFinance = adapter.questions[position]
+
+            when(direction){
+                ItemTouchHelper.RIGHT -> {
+                    viewModel.updateVote(currentFinance, "anwserB")
+                }
+
+                ItemTouchHelper.LEFT -> {
+                    viewModel.updateVote(currentFinance, "anwserA")
+                }
+            }
+            binding.recyclerViewQuestions.adapter?.notifyDataSetChanged()
+        }
+
     }
 
 }
